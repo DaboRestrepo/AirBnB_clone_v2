6ba1c9072c8dc6e22dc08ddc47e1c3b5704f0b81
DaboRestrepo <daborestrepo@gmail.com>
2022-05-23 13:43:22 -0500

    change the permission line

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,6 +13,6 @@ echo '<html>
   </body>
 </html>' | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
 sudo service nginx restart

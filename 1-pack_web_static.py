197ab666559cda41616cf30ce7816d9cadec764e
DaboRestrepo <daborestrepo@gmail.com>
2022-05-23 16:48:09 -0500

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Compress a archive before sending."""
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    """Generate a .tgz file from the web static contents."""
+    if not os.path.exists('versions'):
+        local('mkdir versions')
+    try:
+        time = datetime.now().strftime('%Y%m%d%H%M%S')
+        file_name = 'web_static_' + time + '.tgz'
+        local('tar -cvzf ' + file_name + ' web_static')
+        return file_name
+    except Exception:
+        return None

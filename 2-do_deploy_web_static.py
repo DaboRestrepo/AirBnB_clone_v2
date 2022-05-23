060837a1993a50cdca7233458cc353eca74d5ef3
DaboRestrepo <daborestrepo@gmail.com>
2022-05-23 17:49:25 -0500

    Distribute an archive to both servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module soport the do_deploy function."""
+import os
+from fabric.api import put, run, env
+
+env.hosts = ['35.237.104.173', '34.138.139.5']
+
+
+def do_deploy(archive_path):
+    """Distribute an archive to the web servers."""
+    if not os.path.exists(archive_path):  # archive_path: blabla/blabla
+        return False
+    try:
+        # Split the filename to have only the webserver part.
+        filename = archive_path.split('/')
+        filename = filename[-1]  # web_static_20220523164957.tgz
+        file = filename.split('.')
+        file = file[0]  # without ext.
+        dir = '/data/web_static/releases/' + file  # Folder name
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+        # Uncompress the file.
+        run('mkdir -p ' + dir)
+        run('tar -xzf /tmp/' + filename + '-C' + file)
+        # delete the archive from the web server.
+        run('rm /tmp/' + filename)
+        # delete the current symbolic link.
+        run('unlink /data/web_static/current')
+        # Create a new symbolic link.
+        run('ln -s ' + dir + '/data/web_static/current')
+        return True
+    except Exception:
+        return False

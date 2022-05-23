70ad9f8796259293539fc35acb71970465580444
DaboRestrepo <daborestrepo@gmail.com>
2022-05-23 13:07:53 -0500

    Setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
-# Prepair the web servers
+# Prepare the web servers
 sudo apt update
 sudo apt install nginx -y
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo '<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<p>I am an entry.</p>
-</body>
-</html>' | sudo tee /data/web_static/releases/test/index.html
+echo '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>' | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default

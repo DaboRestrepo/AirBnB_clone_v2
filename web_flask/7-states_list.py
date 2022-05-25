229168e1eb031431bd5bdda8e00985a20260c53f
DaboRestrepo <daborestrepo@gmail.com>
2022-05-25 14:35:55 -0500

    List the states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start a flask app."""
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.rout('/states_list')
+def states():
+    """Display HTML state content."""
+    states = storage.all('State').values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(current):
+    """Remove the current SQL session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
